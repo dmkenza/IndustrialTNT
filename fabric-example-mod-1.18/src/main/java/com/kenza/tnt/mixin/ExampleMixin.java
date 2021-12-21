3ad7b9cba6045b6cfc50364f14f0c2d3e8716e69
dmkenza <66675746+dmkenza@users.noreply.github.com>
2021-12-21 19:55:49 +0300

    prepare Kotlin

--- fabric-example-mod-1.18/src/main/java/com/kenza/tnt/mixin/ExampleMixin.java
+++ fabric-example-mod-1.18/src/main/java/com/kenza/tnt/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package com.kenza.tnt.mixin;
 
-import com.kenza.tnt.ExampleMod;
+import com.kenza.tnt.IndustrialTNTMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		IndustrialTNTMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
---
+++ fabric-example-mod-1.18/src/main/java/com/kenza/tnt/utils.kt
@@ -0,0 +1,44 @@
+package com.kenza.tnt
+
+import com.minelittlepony.common.event.ScreenInitCallback
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.screen.OpenToLanScreen
+import net.minecraft.client.gui.screen.SaveLevelScreen
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.screen.TitleScreen
+import net.minecraft.client.gui.screen.world.CreateWorldScreen
+import net.minecraft.client.sound.PositionedSoundInstance
+import net.minecraft.sound.SoundEvents
+import net.minecraft.text.TranslatableText
+
+
+private var initTitleCounter = 0
+
+
+fun openLastWorldOnInit() {
+    ScreenInitCallback.EVENT.register(ScreenInitCallback { screen: Screen?, buttons: ScreenInitCallback.ButtonList? ->
+        onScreenInit(
+            screen,
+            buttons
+        )
+    })
+}
+
+
+private fun onScreenInit(screen: Screen?, buttons: ScreenInitCallback.ButtonList?) {
+    if (screen is TitleScreen) {
+        //open world after start minecraft
+        initTitleCounter++
+        if (initTitleCounter == 2) {
+            val client = MinecraftClient.getInstance()
+            client.levelStorage.levelList.firstOrNull()?.let { level ->
+                client.soundManager.play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0f))
+                if (client.levelStorage.levelExists(level.name)) {
+                    client.setScreenAndRender(SaveLevelScreen(TranslatableText("selectWorld.data_read")))
+                    client.startIntegratedServer(level.name)
+                }
+            }
+        }
+    }
+}
+
